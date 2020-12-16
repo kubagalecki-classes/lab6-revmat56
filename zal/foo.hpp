83597dd61c15ad02da2e8fad619bd745037d422d
revmat56 <62846638+revmat56@users.noreply.github.com>
2020-12-16 14:05:53 +0100

    Update foo.hpp
    Proba 1

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -11,6 +11,20 @@ std::vector< char > foo(std::list< Human >& people)
     std::vector< char > ret_v(people.size());
 
     // Twoja implementacja tutaj
+    
+    auto funktor =[](Human &y) {
+        y.birthday();
+        if (y.isMonster() == true)
+        {
+        return 'n';
+        }
+        else
+        {
+        return 'y';
+        }
+    };
+
+    std::transform(people.rbegin(), people.rend(),ret_v.begin(),funktor);
 
     return ret_v;
 }
